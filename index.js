c0698e0fb768551a4cb188a995ef3fdb72e855c9
Charles K.S <ctmbr@protonmail.com>
2022-04-25 11:14:12 -0700

    Adding Classes

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 const path = require('path')
 const inquirer = require('inquirer');
 
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
