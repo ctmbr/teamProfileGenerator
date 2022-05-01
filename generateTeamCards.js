3882e71d6c9dec4d898bc8deebfe6063cbaae4d4
Charles K.S <ctmbr@protonmail.com>
2022-04-30 20:44:42 -0700

    Reorganizing directory and stylizing css

--- generateTeamCards.js
+++ generateTeamCards.js
@@ -6,9 +6,9 @@ function generateTeamCards(team) {
     for (let i = 0; i < team.length; i++) {
 
         if (team[i].getRole() === "Engineer") {
-            const engineerCard = `<div>
+            const engineerCard = `<div class = "card-container">
             <h1>Name: ${team[i].getName()}</h1>
-            <div>
+            <div class = "card">
               <p>ID: ${team[i].getId()}</p>
               <p>Email: ${team[i].getEmail()}</p>
               <p>Github: ${team[i].getGithub()}</p>
@@ -17,9 +17,9 @@ function generateTeamCards(team) {
             teamArray.push(engineerCard)
         }
         if (team[i].getRole() === "Intern") {
-            const internCard = `<div>
+            const internCard = `<div class = "card-container">
             <h1>Name: ${team[i].getName()}</h1>
-            <div>
+            <div class = "card">
               <p>ID: ${team[i].getId()}</p>
               <p>Email: ${team[i].getEmail()}</p>
               <p>School: ${team[i].getSchool()}</p>
@@ -28,9 +28,9 @@ function generateTeamCards(team) {
             teamArray.push(internCard)
         }
         if (team[i].getRole() === "Manager") {
-            const managerCard = `<div>
+            const managerCard = `<div class = "card-container">
             <h1>Name: ${team[i].getName()}</h1>
-            <div>
+            <div class = "card">
               <p>ID: ${team[i].getId()}</p>
               <p>Email: ${team[i].getEmail()}</p>
               <p>Office Number: ${team[i].getOfficeNumber()}</p>
